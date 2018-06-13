4f2a53c43aa3d1a5fdac0c1cb374a62246efe1f6
Marcos Cesar Gritti <cesargritti@gmail.com>
2018-06-13 12:18:04 -0300

    Link loader added

--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,23 @@
-function alreadyLoaded (url) {
+function scriptAlreadyLoaded (url) {
   return (document.head.querySelector(`script[src="${url}"]`) !== null)
 }
 
-function getUrl (options) {
+function linkAlreadyLoaded (href) {
+  return (document.head.querySelector(`link[href="${href}"]`) !== null)
+}
+
+function getField (options, field) {
   if (typeof options === 'string') return options
-  if (!options.src) throw new Error('vue-scriptload:loadScript needs an url')
-  return options.src
+  if (!options[field]) throw new Error(`vue-scriptload:Missing ${field} from options`)
+  return options[field]
+}
+
+function getUrl (options) {
+  return getField(options, 'src')
+}
+
+function getHref (options) {
+  return getField(options, 'href')
 }
 
 function configureScript (script, options) {
@@ -22,9 +34,24 @@ function configureScript (script, options) {
   }
 }
 
-function promiseLoader (options) {
+function configureLink (link, options) {
+  if (typeof options === 'string') {
+    link.rel = 'stylesheet'
+    link.type = 'text/css'
+    link.href = options
+    link.async = true
+  } else {
+    const defaults = { rel: 'stylesheet', type: 'text/css', async: true }
+    const merge = Object.assign(defaults, options)
+    for (let key in merge) {
+      link[key] = merge[key]
+    }
+  }
+}
+
+function scriptPromiseLoader (options) {
   const url = getUrl(options)
-  if (alreadyLoaded(url)) return Promise.resolve()
+  if (scriptAlreadyLoaded(url)) return Promise.resolve()
   return new Promise((resolve, reject) => {
     const script = document.createElement('script')
     configureScript(script, options)
@@ -34,8 +61,28 @@ function promiseLoader (options) {
   })
 }
 
-function callbackLoader (options, callback) {
-  promiseLoader(options).then(() => {
+function linkPromiseLoader (options) {
+  const href = getHref(options)
+  if (linkAlreadyLoaded(href)) return Promise.resolve()
+  return new Promise((resolve, reject) => {
+    const link = document.createElement('link')
+    configureLink(link, options)
+    link.onload = resolve
+    link.onerror = reject
+    document.head.appendChild(link)
+  })
+}
+
+function scriptCallbackLoader (options, callback) {
+  scriptPromiseLoader(options).then(() => {
+    callback(null)
+  }).catch(err => {
+    callback(err)
+  })
+}
+
+function linkCallbackLoader (options, callback) {
+  linkPromiseLoader(options).then(() => {
     callback(null)
   }).catch(err => {
     callback(err)
@@ -43,12 +90,18 @@ function callbackLoader (options, callback) {
 }
 
 export function loadScript (options, callback) {
-  if (typeof callback === 'function') return callbackLoader(options, callback)
-  else return promiseLoader(options)
+  if (typeof callback === 'function') return scriptCallbackLoader(options, callback)
+  else return scriptPromiseLoader(options)
+}
+
+export function loadLink (options, callback) {
+  if (typeof callback === 'function') return linkCallbackLoader(options, callback)
+  else return linkPromiseLoader(options)
 }
 
 export default {
   install (Vue, options) {
     Vue.prototype.$loadScript = loadScript
+    Vue.prototype.$loadLink = loadLink
   }
 }
